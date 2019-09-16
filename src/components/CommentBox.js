f80cfd1401e65bb3bb9b4dc5304eb079035b3018
Frank Navarro <fnavarro7777@gmail.com>
2019-09-16 16:32:48 -0700

    Wired up actions and reducers

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveComment } from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -12,6 +14,7 @@ class CommentBox extends Component {
 
     // TODO - Call an action creator
     // And save the comment
+    this.props.saveComment(this.state.comment);
 
     this.setState({ comment: '' });
   };
@@ -29,4 +32,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  { saveComment },
+)(CommentBox);

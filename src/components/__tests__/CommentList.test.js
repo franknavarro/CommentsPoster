de32b10bad4217b4d61b7523612abb856399c20e
Frank Navarro <fnavarro7777@gmail.com>
2019-09-17 12:24:42 -0700

    Tests for Comment List

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+let wrapped;
+beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2'],
+  };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>,
+  );
+});
+
+it('creates on li per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
+});
+
+it('shows the text for each comment', () => {
+  const wrappedText = wrapped.render().text();
+  expect(wrappedText).toContain('Comment 1');
+  expect(wrappedText).toContain('Comment 2');
+});

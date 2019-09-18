652872ae95ad66a264821356a0a0a2d550b34ea5
Frank Navarro <fnavarro7777@gmail.com>
2019-09-18 12:15:28 -0700

    Added menu for navigation and sign on button

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,76 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components//CommentList';
+import { changeAuth } from 'actions';
 
-export default () => {
-  return (
-    <div className="ui container">
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
+class App extends Component {
+  renderSignInButton() {
+    let button;
+    if (this.props.auth) {
+      button = {
+        newState: false,
+        text: 'Sign Out',
+        color: 'red',
+      };
+    } else {
+      button = {
+        newState: true,
+        text: 'Sign In',
+        color: 'primary',
+      };
+    }
+
+    return (
+      <div className="item">
+        <button
+          className={`ui button ${button.color}`}
+          onClick={() => this.props.changeAuth(button.newState)}
+        >
+          {button.text}
+        </button>
+      </div>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <div className="ui borderless menu fixed">
+        <div className="ui container grid">
+          <div className="row">
+            <Link to="/" className="item">
+              Home
+            </Link>
+            <Link to="/post" className="item">
+              Post
+            </Link>
+            <div className="right menu">{this.renderSignInButton()}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <div className="ui container" style={{ marginTop: '5rem' }}>
+          <Route path="/post" component={CommentBox} />
+          <Route path="/" exact component={CommentList} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { auth: state.auth };
 };
+
+export default connect(
+  mapStateToProps,
+  { changeAuth },
+)(App);

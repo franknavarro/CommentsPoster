be2fa2ae377d26dedd81ed407dfa5c12db052947
Frank Navarro <fnavarro7777@gmail.com>
2019-09-18 13:21:10 -0700

    Hid posts button when not signed in

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
-import CommentList from 'components//CommentList';
+import CommentList from 'components/CommentList';
 import { changeAuth } from 'actions';
+import renderAuth from 'components/renderAuth';
 
 class App extends Component {
   renderSignInButton() {
@@ -35,6 +36,17 @@ class App extends Component {
     );
   }
 
+  renderPostsButton() {
+    const PostButton = () => {
+      return (
+        <Link to="/post" className="item">
+          Post
+        </Link>
+      );
+    };
+    return renderAuth(PostButton);
+  }
+
   renderHeader() {
     return (
       <div className="ui borderless menu fixed">
@@ -43,9 +55,7 @@ class App extends Component {
             <Link to="/" className="item">
               Home
             </Link>
-            <Link to="/post" className="item">
-              Post
-            </Link>
+            {this.renderPostsButton()}
             <div className="right menu">{this.renderSignInButton()}</div>
           </div>
         </div>

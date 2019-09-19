a8ff8de3a0cd292e437132e65b13495df71328c0
Frank Navarro <fnavarro7777@gmail.com>
2019-09-19 12:35:43 -0700

    Created a middleware to validate the redux store values

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import async from 'middlewares/async';
 
+import async from 'middlewares/async';
+import storeValidator from 'middlewares/storeValidator';
 import reducers from 'reducers';
 
 const Root = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, storeValidator),
+  );
 
   return <Provider store={store}>{children}</Provider>;
 };
